6044b3da94d303f678eec87a76bb73e8c689b68c
kodeline <linekode@gmail.com>
2026-09-08 20:12:24 -0300

    clase practica 4 agregada

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -10,10 +10,6 @@ const Bienvenida = () => {
     { nombre: "Carlos Ruiz", tarea: "Backend Developer", emoji: "" },
   ];
 
-  const [numero, setNumero] = useState(0);
-  const [error, setError] = useState(false);
-  const [estaBien, setEstaBien] = useState(false);
-
   return (
     <div>
       <Titulos>
--- src/components/products/Item.jsx
+++ src/components/products/Item.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
+import BotonFavorito from "../BotonFavorito"
+
 const Item = ({nombre, precio}) => {
-  return(
+
+  const [contador, setContador] = useState(0);
+  const incrementar = () => { setContador(contador + 1) };
+  
+  const decrementar = () => {
+    if(contador > 0) 
+      setContador(contador - 1) 
+  };
+
+  return (
     <div>
-      <h2>{nombre}: {precio}</h2>
+      <h2>
+        {nombre}: AR${precio}
+      </h2>
+      <BotonFavorito />
+      <button onClick={decrementar}> - </button>
+      <p>{contador}</p>
+      <button onClick={incrementar}> + </button>
     </div>
-  )
+  );
 }
 
 export default Item
